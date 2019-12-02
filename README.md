9dd1c822f6c75d49e67643c9d1b01e047199bdfb
Melanie Jouaiti <melanie.jouaiti@gmail.com>
2019-12-02 16:11:14 +0100

    multiple person detection

--- README.md
+++ README.md
@@ -4,6 +4,7 @@ Pose estimation for Mac using CoreML
 Installation:
 pip install tfcoreml
 pip install coremltools
+cd pafprocess && swig -python -c++ pafprocess.i && python2 setup.py build_ext --inplace
 
 CoreML does not support variable resolution so you have to generate a new model if you want to change it. The conversion code is provided in tfcoreML.py, just change the dim_x and dim_y values.
 
@@ -12,7 +13,5 @@ Inference Time on a Radeon Pro Vega 20 4 GB:
 640 x 640: 55 ms
 
 We also provide some example code on how to use the model. 
-This only works on single person detection.
 
-
-In the future, we will try to handle multiple person detection.
+The code handles multiple person detection thanks to pafprocess (https://github.com/ildoonet/tf-pose-estimation/).
--- src/test_video_2d.py
+++ src/test_video_2d.py
@@ -2,8 +2,18 @@ import coremltools
 import numpy as np
 from PIL import Image, ImageDraw
 import time
+import matplotlib.pyplot as plt
 import cv2
 import sys
+
+
+try:
+    from pafprocess import pafprocess
+except ModuleNotFoundError as e:
+    print(e)
+    print('you need to build c++ library for pafprocess. See : https://github.com/ildoonet/tf-pose-estimation/tree/master/tf_pose/pafprocess')
+    exit(-1)
+    
 
 #0: "top",              ---> 1
 #1: "neck",             ---> 2, 8, 5, 11
@@ -20,29 +30,39 @@ import sys
 #12: "L knee"           ---> 13
 #13: "L ankle"
 
+DISPLAY = 1
+
 connections = [[0, 1], [1, 2], [1, 5], [1, 8], [1, 11],
                 [2, 3], [3, 4], [5, 6], [6, 7],
                 [8, 9], [9, 10], [11, 12], [12, 13]]
 
-DISPLAY = 1
-
-enc_in = np.zeros((1, 64))
-enc_in[0] = [0 for i in range(64)]
-
-dt = 0.04
-poses2d = []
-
 if __name__ == '__main__':
+    
+    frame = 0
+    
     # Load the model
-    model =  coremltools.models.MLModel("models/graph/mobilenet_thin/graph320x320.mlmodel")
+    model =  coremltools.models.MLModel("/Users/Melanie/tf-pose-estimation/models/graph/mobilenet_thin/graph640.mlmodel")
 
     cap = cv2.VideoCapture(sys.argv[1])
+#    cap = cv2.VideoCapture(0)
+    
+    hog = cv2.HOGDescriptor()
+    hog.setSVMDetector(cv2.HOGDescriptor_getDefaultPeopleDetector())
 
-    dim = 320
-    out_dim = 40
+    dim = 640
+    out_dim = 80
     scale = 8
     key = "Openpose__concat_stage7__0"
     
+    p_top, p_neck = 0, 0
+    p_rShoulder, p_lShoulder = 0, 0
+    p_rElbow, p_lElbow= 0, 0
+    p_rWrist, p_lWrist = 0, 0
+    p_rHip, p_lHip = 0, 0
+    p_rKnee, p_lKnee = 0, 0
+    p_rAnkle, p_lAnkle = 0, 0
+    
+    plt.figure()
     while cap.isOpened():
         _, img = cap.read()
         t = time.time()
@@ -50,17 +70,68 @@ if __name__ == '__main__':
         resized = img.resize((dim, dim), Image.LINEAR)
         predictions = model.predict({"image__0": resized}, False)
         
-        preds = [np.mean(np.argwhere(predictions[key][i] > 0.1), axis=0)[::-1] for i in range(18)]
-        preds_t = [tuple(int(scale * x) for x in pred) for pred in preds]
-        _data = np.array([[preds_t[i][0], preds_t[i][1]] for i in range(18)]).flatten()
+        heatMat2 = predictions[key][:19,:,:]
+        heatMat = np.zeros((80, 80, 19))
+        for i in range(19):
+            heatMat[:,:, i] = heatMat2[i,:,:]
+        pafMat = np.zeros((80, 80, 38))
+        pafMat2 = predictions[key][19:,:,:]
+        for i in range(38):
+            pafMat[:,:, i] = pafMat2[i,:,:]
+        
+        
+        print time.time() - t
+        
+        preds = [np.mean(np.argwhere(heatMat[:,:,i] > 0.1), axis=0)[::-1] for i in range(19)]
+        preds_t = []
+        for pred in preds:
+            try:
+                preds_t.append(tuple(int(x) for x in pred))
+            except:
+                preds_t.append((0, 0))
+
+        peaks = np.zeros((80, 80, 19))
+        for i in range(19):
+            peaks[preds_t[i][1], preds_t[i][0], i] = heatMat[preds_t[i][1], preds_t[i][0], i]
+        
+        pafprocess.process_paf(peaks.astype('float32'), heatMat.astype('float32'), pafMat.astype('float32'))
+        
+        humans = []
+        for human_id in range(pafprocess.get_num_humans()):
+            body_parts = [(0.0, 0.0)] * 18
+            for part_idx in range(18):
+                c_idx = int(pafprocess.get_part_cid(human_id, part_idx))
+                if c_idx < 0:
+                    continue
+
+                is_added = True
+                body_parts[part_idx] = (pafprocess.get_part_x(c_idx),
+                                        pafprocess.get_part_y(c_idx))
+            body_parts = np.array([[body_parts[i][0] * scale, body_parts[i][1] * scale] for i in range(18)]).flatten()
+            humans.append(body_parts)
         
         d = ImageDraw.Draw(resized)
         
-        for pair in connections:
-            d.line([(_data[pair[0] * 2], _data[pair[0] * 2 + 1]),   (_data[pair[1] * 2], _data[pair[1] * 2 + 1])], fill=(0, 0, 255), width=2)
+        for _data in humans:
+            for pair in connections:
+                if (_data[pair[0] * 2], _data[pair[0] * 2 + 1]) != (0, 0) and (_data[pair[1] * 2], _data[pair[1] * 2 + 1]) != (0, 0):
+                    d.line([(_data[pair[0] * 2], _data[pair[0] * 2 + 1]), (_data[pair[1] * 2], _data[pair[1] * 2 + 1])], fill=(255, 255, 255), width=2)
 
-        print "Inference time: ", time.time() - t
-        resized = np.array(resized)
+        print time.time() - t
+        
+        if DISPLAY:
+            resized = np.array(resized)
+    
+            plt.subplot(131)
+            plt.imshow(np.sum(heatMat[:,:,i] for i in range(19)))
+            plt.subplot(132)
+            plt.imshow(np.sum(pafMat[:,:,i] for i in range(19)))
+            plt.subplot(133)
+            plt.imshow(np.sum(peaks[:,:,i] for i in range(19)))
+            plt.draw()
+            plt.pause(0.001)
+    
+            cv2.imshow("result", resized)
+            cv2.waitKey(1)
+                    
 
-        cv2.imshow("window", resized)
-        cv2.waitKey(1)
